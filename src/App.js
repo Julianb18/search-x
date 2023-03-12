4c0ff8349ae35e7d63b4ae475cc2bdd7651c03bf
Julian Braun <julian141718@gmail.com>
2023-03-12 16:33:54 +0200

    add basic input functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Searchbar } from "./components/Searchbar";
+import data from "./Data.json";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="h-screen bg-[#202124] p-7">
+      <h1 className="text-3xl font-bold text-pink-600">Hello world</h1>
+      <Searchbar data={data} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Searchbar = () => {
-  return <div>Searchbar</div>;
+export const Searchbar = ({ data }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleOnChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSearch = (searchText) => {
+    setInputValue(searchText);
+    console.log(searchText);
+  };
+
+  return (
+    <div className="max-w-xl">
+      <div className="relative w-full">
+        <input
+          className="w-full text-white border border-[#9CA3AF] caret-white outline-none rounded-3xl pl-4 py-2 pr-[71px] bg-transparent hover:bg-[#303134] hover:border-none"
+          type="text"
+          value={inputValue}
+          onChange={handleOnChange}
+        />
+        <div className="flex absolute h-full rounded-r-3xl min-w-[55px] top-0 right-0">
+          <div className="h-3/5 w-[1px] self-center bg-[#9CA3AF]"></div>
+
+          <button
+            className="flex justify-center items-center text-white rounded-r-3xl w-full"
+            onClick={() => onSearch(inputValue)}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
